7665afe02d0e82e430637634d90944ceaae63e40
Bronx972 <ricardo.guerrero@tecsup.edu.pe>
2023-05-28 16:12:11 -0500

    Agregar archivos de proyecto.

---
+++ Lab8_b/Lab8_b/Dinámico.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab8_b
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Dinámico : ContentPage
+    {
+        bool originalStyle = true;
+
+        public Dinámico()
+        {
+            InitializeComponent();
+            Resources["searchBarStyle"] = Resources["blueSearchBarStyle"];
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (originalStyle)
+            {
+                Resources["searchBarStyle"] = Resources["greenSearchBarStyle"];
+                originalStyle = false;
+            }
+            else
+            {
+                Resources["searchBarStyle"] = Resources["blueSearchBarStyle"];
+                originalStyle = true;
+            }
+        }
+    }
+}
